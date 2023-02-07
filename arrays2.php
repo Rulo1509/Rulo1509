fa7f69b927369fcfa0fb45dd3e92486180e393b7
Rulo1509 <119310389+Rulo1509@users.noreply.github.com>
2023-02-07 20:52:33 +0100

    Update arrays2.php

--- arrays2.php
+++ arrays2.php
@@ -1,1 +1,15 @@
-
+<?php
+	$nota = array (0,1,2);
+	
+	//Insertar valor al final del array
+	$nota[] = 3;
+	echo "Apartado C: ";
+	print_r($nota);
+	
+	echo "<br>";
+	//Eliminar el primer valor
+	unset($nota[0]);
+	echo "Apartado D: ";
+	print_r($nota);
+	
+?>
