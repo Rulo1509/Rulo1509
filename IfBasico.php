44f8d45d0510ea7cf17e71fecd9404e5c37c19ab
Rulo1509 <119310389+Rulo1509@users.noreply.github.com>
2023-02-07 20:53:47 +0100

    Update IfBasico.php

--- IfBasico.php
+++ IfBasico.php
@@ -1,1 +1,16 @@
+<?php
+$nome = "Raúl";
+$idade = -17;
 
+
+if ($idade >= 18) {
+	echo "El ciudadano ".$nome." puede votar ";
+	echo date("d/m/Y");
+}
+elseif ($idade < 18 and $idade >= 0) {
+	echo "El ciudadano ".$nome." no puede puede votar porque es menor de edad ";
+	echo date("d/m/Y");
+}
+else 
+	echo "El dato introducido es incorrecto, por favor introduzca un número positivo";
+?>
